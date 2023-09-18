e6bbe19ff7028df3531f418a4c99fb32cd2b76a9
Aheesh <54215577+aheesh13@users.noreply.github.com>
2023-09-18 19:38:34 +0530

    Delete redbus directory

--- redbus/src/main/java/com/bus/redbus/repository/BusDateAndDestRepo.java
+++
@@ -1,9 +0,0 @@
-package com.bus.redbus.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.bus.redbus.model.BusDateAndDestination;
-
-public interface BusDateAndDestRepo extends JpaRepository<BusDateAndDestination, Long>{
-
-}
